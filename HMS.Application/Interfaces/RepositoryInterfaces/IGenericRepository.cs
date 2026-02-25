991483bb9c99e6a04613b44578b28ee1292a931d
Magda GvirjiSvili <magdagvirjishvili@gmail.com>
2026-02-25 18:31:25 +0400

    add generic and hotels reposiotories and interfaces

---
+++ HMS.Application/Interfaces/RepositoryInterfaces/IGenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace HMS.Application.Interfaces.RepositoryInterfaces;
+
+public interface IGenericRepository<T, TContext>
+    where T : class
+    where TContext : DbContext
+{
+    Task AddAsync(T entity);
+    void Remove(T entity);
+    void RemoveRange(IEnumerable<T> entitites);
+    void Update(T entity);
+    Task<int> SaveAsync(CancellationToken cancellationToken = default);
+    Task<bool> ExistsAsync(Expression<Func<T, bool>> predicate);
+    Task<(List<T> Items, int TotalCount)> GetAllAsync(
+        Expression<Func<T, bool>> filter = null,
+        int? pageNumber = null,
+        int? pageSize = null,
+        string orderBy = null,
+        bool ascending = true,
+        Func<IQueryable<T>, IQueryable<T>> includes = null,
+        bool tracking = true);
+    Task<T> GetAsync(
+        Expression<Func<T, bool>> filter,
+        Func<IQueryable<T>, IQueryable<T>> includes = null,
+        bool tracking = true);
+}
+
+
---
+++ HMS.Infrastructure/Repositories/HotelRepository.cs
@@ -0,0 +1,12 @@
+﻿using HMS.Application.Interfaces.RepositoryInterfaces;
+using HMS.Domain.Entities;
+using HMS.Infrastructure.Data;
+
+namespace HMS.Infrastructure.Repositories;
+
+public class HotelRepository: GenericRepository<Hotel, AppDbContext>, IHotelRepository
+{
+    public HotelRepository(AppDbContext context) : base(context)
+    {
+    }
+}
